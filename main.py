4ac43207ec51772f402a3049c5ca1aeaa24206f6
Mary TRAORE <102195748+marytraore02@users.noreply.github.com>
2024-05-22 23:43:23 +0000

    first version 001 for web-app

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-from utils import *
+from utils import CnnClassifier, get_category
 
 
 # for custom CSS style
@@ -11,5 +11,59 @@ with open("style.css") as f:
 
 st.header("Web-app-text-classification")
 
-print("my file")
-print("test2")
+with st.form(key="input_form"):
+    options = st.multiselect(
+        'Selectionner un model',
+        ['CNN'])
+
+    text = st.text_area(label="Text d'entrer", height=200,
+    placeholder="Entrez le texte")
+    
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        if text != "":
+            with st.spinner("Your text is being predicted..."):
+                if "CNN" in options and len(options) == 1:
+                    model = CnnClassifier()
+                    text_vector = model.convert_text_to_vector(text)
+                    predictions = model.predict(text_vector)
+
+                # elif "GRU" in options and len(options) == 1:
+                #     model = GruClassifier()
+                #     text_vector = model.convert_text_to_vector(text)
+                #     predictions = model.predict(text_vector)
+
+                # elif len(options) == 2:
+                #     lstm_model = LstmClassifier()
+                #     lstm_text_vector = lstm_model.convert_text_to_vector(text)
+                #     lstm_predictions = lstm_model.predict(lstm_text_vector)
+
+                #     gru_model = GruClassifier()
+                #     gru_text_vector = gru_model.convert_text_to_vector(text)
+                #     gru_predictions = gru_model.predict(gru_text_vector)
+
+                #     predictions = (lstm_predictions + gru_predictions) / 2
+
+                else:
+                    st.error(
+                        "Veuillez sélectionner un modèle à prédire")
+                    st.stop()
+
+            col1, col2 = st.columns(2)
+            with col1:
+                predicted_category = get_category(predictions)
+                st.write("Catégorie prédit")
+                st.success(predicted_category)
+
+            with col2:
+                st.write("Score de prédiction:")
+                st.success(f"{np.max(predictions)*100:.2f} %")
+
+            st.write("Graphique de prédiction:")
+            df = pd.DataFrame(data=predictions, columns=[
+                              "Business", "Entertainment", "Politics", "Sports", "Technology"])
+            st.bar_chart(df.T, height=550)
+        else:
+            st.error(
+                'Veuillez saisir un texte qui souhaite être prédit')
+
--- utils.py
+++ utils.py
@@ -4,13 +4,77 @@ import pandas as pd
 from tensorflow import keras
 from keras.utils import pad_sequences
 
-
-idx2category = {
-  0: "Business",
-  1: "Entertainment",
-  2: "Politics",
-  3: "Sports",
-  4: "Technology"
+# Déclaration d'un dictionnaire qui prend les catégories et leurs index
+dict_category = {
+    0: "Business",
+    1: "Entertainment",
+    2: "Politics",
+    3: "Sports",
+    4: "Technology"
 }
 
-print('mary')
+def get_category(predictions: np.ndarray) -> str:
+  """
+  Get the category of the maximum score in `predictions`.
+
+  Parameters:
+  ------------
+  - predictions : np.ndarray
+      Contains 5 scores repesenting the predcited score for each category.
+
+  Returns:
+  --------
+  - str
+      Represents the predicted score using `dict_category` dictionary.
+  """
+  predicted_index = np.argmax(predictions)
+  return dict_category[predicted_index]
+
+
+class CnnClassifier:
+  """Responsible for make predictions on a text using LSTM-based model"""
+
+  def __init__(self) -> None:
+      self.cnn_model = keras.models.load_model(
+          "models/cnn_model.h5")
+      with open("tokenizers/tokenizer.pickle", 'rb') as handle:
+          self.cnn_tokenizer = pickle.load(handle)
+
+  def convert_text_to_vector(self, text: str) -> np.ndarray:
+      """
+      Convert passed `text` to a numerical vector.
+
+      Parameters:
+      ------------
+      - text : str
+          Text needed to be converted to vector.
+
+      Returns:
+      --------
+      - text_vector_padded : np.ndarray
+          A vector representation of size 256 of `text`.
+      """
+      text = pd.Series(text)
+      text_vector = self.cnn_tokenizer.texts_to_sequences(text)
+      text_vector_padded = pad_sequences(
+          text_vector, maxlen=500, padding="post", truncating="post")
+
+      return text_vector_padded
+
+  def predict(self, text_vector: np.ndarray) -> np.ndarray:
+      """
+      Get predictions scores of the passes `text_vector`
+
+      Parameters:
+      ------------
+      - text_vector : np.ndarray
+          A vector representation of a text of size 256
+
+      Returns:
+      --------
+      - predictions : np.ndarray
+          A vector of size number of categories "5" represents
+          the score of each category.
+      """
+      predictions = self.cnn_model.predict(text_vector)
+      return predictions
